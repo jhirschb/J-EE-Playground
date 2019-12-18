11fff22d36c690610532326c3f5f05df8764f8b2
jhirschbeck <jhirschbeck@2nd-step.de>
2019-12-18 21:15:49 +0100

    adding swagger/openapi

---
+++ JEE-JPA/src/main/java/playground/ee/rest/CorsFilter.java
@@ -0,0 +1,28 @@
+package playground.ee.rest;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+/**
+ * @author jhirschbeck
+ */
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+
+        responseContext.getHeaders().add(
+            "Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add(
+            "Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add(
+            "Access-Control-Allow-Headers",
+            "origin, content-type, api-key, accept, authorization");
+        responseContext.getHeaders().add(
+            "Access-Control-Allow-Methods",
+            "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
--- JEE-JPA/src/main/java/playground/ee/rest/RestApplication.java
+++ JEE-JPA/src/main/java/playground/ee/rest/RestApplication.java
@@ -1,12 +1,47 @@
 package playground.ee.rest;
 
+import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.integration.OpenApiConfigurationException;
+import io.swagger.v3.oas.integration.SwaggerConfiguration;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletConfig;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author jhirschbeck
  */
 @ApplicationPath("/api")
+@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080/jee-jpa")})
 public class RestApplication extends Application {
 
+    public static final Logger LOG = LoggerFactory.getLogger(RestApplication.class);
+
+    public RestApplication(@Context ServletConfig servletConfig) {
+        OpenAPI oas = new OpenAPI();
+        SwaggerConfiguration oasConfig = new SwaggerConfiguration()
+            .openAPI(oas)
+            .prettyPrint(true)
+            .resourcePackages(Stream.of("playground.ee.rest").collect(Collectors.toSet()));
+
+        try {
+            LOG.info("initializing openapi");
+            new JaxrsOpenApiContextBuilder()
+                .servletConfig(servletConfig)
+                .application(this)
+                .openApiConfiguration(oasConfig)
+                .buildContext(true);
+        } catch (OpenApiConfigurationException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+    }
 }
